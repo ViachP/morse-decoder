26ad2b43f8513f7071b8bff885de47b51e24e4c2
Viachaslau Pailak <76894715+ViachP@users.noreply.github.com>
2022-02-25 02:04:28 +0300

    feat: added decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+      let array = [];
+  for (let i = 0; i < expr.length; i = i + 10) {
+    let unit = expr.substring(i, i + 10);
+    if (unit.includes('*')) {
+      array.push(' ');
+      continue;
+    }
+    unit = unit.replace(/11/g, '-');
+    unit = unit.replace(/10/g, '.');
+    unit = unit.replace(/00/g, '');
+    array.push(MORSE_TABLE[unit]);
+
+  }
+  return array.join('');
+
 }
 
 module.exports = {
